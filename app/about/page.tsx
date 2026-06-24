2a6ef3c62c94c946edcd0f2a2e5a5fe31f232225
Sjain0018 <sj5373557@gmail.com>
2026-06-25 01:17:12 +0530

    add header

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,15 +3,19 @@ import React from 'react';
 import VerticalAlternatingTimeline from '@/components/VerticalAlternatingTimeline';
 import Teamcore from '@/components/Teamcore';
 import FloatingActionButton from '@/components/FloatingActionButton';
+import Header from '@/components/Header';
 
 const AboutPage: React.FC = () => {
   return (
+    <div>
+        <Header />
     <div className="min-h-screen bg-gray-50">
       <div className="container mx-auto px-4 py-8">
         <VerticalAlternatingTimeline />
         <Teamcore />
         <FloatingActionButton />
       </div>
+    </div>
     </div>
   );
 };
