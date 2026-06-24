9d5e2390790cde1c208e30fac6e39d90b2df7192
bhavy-sharma <bhavymay18@gmail.com>
2026-06-25 01:09:32 +0530

    Team half

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 import Header from "@/components/Header";
 import HeroSection from "@/components/Home/HeroSection";
+import Stats from "@/components/Home/Stats";
 
 export default function Page() {
   return (
     <>
       <Header />
       <HeroSection />
+      <Stats />
     </>
   );
 }
