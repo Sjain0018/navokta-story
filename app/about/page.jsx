8d60d4958896cc32eef020411aa1d0da215c9be0
bhavy-sharma <bhavymay18@gmail.com>
2026-06-25 02:16:14 +0530

    Fix about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,16 +1,17 @@
 "use client";
-import React from 'react';  
-import Header from '@/components/Header';
-import VerticalAlternatingTimeline from '@/components/About/VerticalAlternatingTimeline';
-import Teamcore from '@/components/About/Teamcore';
+import React from "react";
+import Header from "@/components/Header";
+import VerticalAlternatingTimeline from "@/components/About/VerticalAlternatingTimeline";
+import Teamcore from "@/components/About/Teamcore";
 
 const AboutPage = () => {
   return (
     <div>
-    <VerticalAlternatingTimeline />
-   <Teamcore />
-   </div>
-  )
-}
+      <Header />
+      <VerticalAlternatingTimeline />
+      <Teamcore />
+    </div>
+  );
+};
 
 export default AboutPage;
