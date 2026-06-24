7c14f5ca1da524c0f25caaca72a146f8a6a45f65
bhavy-sharma <bhavymay18@gmail.com>
2026-06-25 01:02:58 +0530

    Header

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+import HeroSection from "@/components/Home/HeroSection";
+
+export default function Page() {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+    </>
+  );
+}
